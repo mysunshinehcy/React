57addb9788ce7becbee706dbcffb60ec30f63dad
mysunshinehcy <1042006984@qq.com>
2018-12-02 12:26:33 +0800

    Updates

---
+++ precache-manifest.17b7f8faca926a4c03ca7bfd2781e1de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "14d5b2fe69ddf300f5b9",
+    "url": "/React/static/js/main.14d5b2fe.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/React/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "14d5b2fe69ddf300f5b9",
+    "url": "/React/static/css/main.5d9bf31d.chunk.css"
+  },
+  {
+    "revision": "061b7b8f84376ef5082d95ca3c322c83",
+    "url": "/React/index.html"
+  }
+];
